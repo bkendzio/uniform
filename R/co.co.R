7339d9015d680fa253949038beb119924826005b
BKMac <bkmac@bkmacs-mbp.localdomain>
2023-02-01 22:00:30 +0100

    add co.co function

---
+++ R/co.co.R
@@ -0,0 +1,5 @@
+#' @title co.co
+#'
+#' @description Uniforms a differently or incompletely reported continuous outcome across studies included in a meta-analysis
+#'
+#' @param study A vector with study labels
